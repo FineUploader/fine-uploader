27cbcf4fd6192933dfa486c819fc8915d504ecf3
Stjepan Zlodi <lupus@lupus.hr>
2012-04-17 11:42:18 +0200

    Added customHeader option for additional headers (usable for X-CSRFToken in Django applications)

--- client/fileuploader.js
+++ client/fileuploader.js
@@ -254,6 +254,7 @@ qq.FileUploaderBasic = function(o){
         debug: false,
         action: '/server/upload',
         params: {},
+        customHeaders: {},
         button: null,
         multiple: true,
         maxConnections: 3,
@@ -324,6 +325,7 @@ qq.FileUploaderBasic.prototype = {
             debug: this._options.debug,
             action: this._options.action,         
             maxConnections: this._options.maxConnections,   
+            customHeaders: this._options.customHeaders,
             onProgress: function(id, fileName, loaded, total){                
                 self._onProgress(id, fileName, loaded, total);
                 self._options.onProgress(id, fileName, loaded, total);                    
@@ -1201,6 +1203,9 @@ qq.extend(qq.UploadHandlerXhr.prototype, {
         xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
         xhr.setRequestHeader("X-File-Name", encodeURIComponent(name));
         xhr.setRequestHeader("Content-Type", "application/octet-stream");
+        for (key in this._options.customHeaders){
+            xhr.setRequestHeader(key, this._options.customHeaders[key]);
+        };
         xhr.send(file);
     },
     _onComplete: function(id, xhr){
