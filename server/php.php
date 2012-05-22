43949479e50fbaeb92797a05bd20e584c6bf8ab5
Simon East <simon@surfacemedia.com.au>
2012-05-22 13:20:10 +1000

    Set PHP code to be purely an example only, not actually perform the upload (to allow customisation of the settings)

--- server/php.php
+++ server/php.php
@@ -1,5 +1,27 @@
 <?php
 
+/****************************************
+Example of how to use this uploader class...
+You can uncomment the following lines (minus the require) to use these as your defaults.
+
+// list of valid extensions, ex. array("jpeg", "xml", "bmp")
+$allowedExtensions = array();
+// max file size in bytes
+$sizeLimit = 10 * 1024 * 1024;
+
+require('valums-file-uploader/server/php.php');
+$uploader = new qqFileUploader($allowedExtensions, $sizeLimit);
+
+// Call handleUpload() with the name of the folder, relative to PHP's getcwd()
+$result = $uploader->handleUpload('uploads/');
+
+// to pass data through iframe you will need to encode all html tags
+echo htmlspecialchars(json_encode($result), ENT_NOQUOTES);
+
+/******************************************/
+
+
+
 /**
  * Handle file uploads via XMLHttpRequest
  */
@@ -81,6 +103,11 @@ class qqFileUploader {
         }
     }
     
+	public function getName(){
+		if ($this->file)
+			return $this->file->getName();
+	}
+    
     private function checkServerSettings(){        
         $postSize = $this->toBytes(ini_get('post_max_size'));
         $uploadSize = $this->toBytes(ini_get('upload_max_filesize'));        
@@ -150,13 +177,3 @@ class qqFileUploader {
         
     }    
 }
-
-// list of valid extensions, ex. array("jpeg", "xml", "bmp")
-$allowedExtensions = array();
-// max file size in bytes
-$sizeLimit = 10 * 1024 * 1024;
-
-$uploader = new qqFileUploader($allowedExtensions, $sizeLimit);
-$result = $uploader->handleUpload('uploads/');
-// to pass data through iframe you will need to encode all html tags
-echo htmlspecialchars(json_encode($result), ENT_NOQUOTES);
