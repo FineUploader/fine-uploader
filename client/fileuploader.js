e7ebfbe875930c23927867d319033ddc5fb21b6e
Ray Nicholus <rnicholus@widen.com>
2012-09-20 21:26:29 -0500

    Revert "This reverts commit f4fc80b44af4c1d9993df9ae37d6faffd10463d4."
    This reverts commit b44d14b78e51aa2eb3d4628d755876d90ce657c8.

--- client/fileuploader.js
+++ client/fileuploader.js
@@ -1278,6 +1278,7 @@ qq.extend(qq.UploadHandlerForm.prototype, {
         }
 
         var fileName = this.getName(id);
+        params[this._options.inputName] = fileName;
 
         var iframe = this._createIframe(id);
         var form = this._createForm(iframe, params);
