d95f5a42568392b3bd078050a511fda2b1a0fd42
Pablo Martín <goinnn@gmail.com>
2012-10-23 17:44:49 +0200

    pep8, pyflakes, rm .pyc, rename view, add upload dir, remove wrong settings

--- server/python_django/file_uploader/settings.py
+++ server/python_django/file_uploader/settings.py
@@ -11,8 +11,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/six519/workspace/file_uploader/src/sqlite.db',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
--- server/python_django/file_uploader/views.py
+++ server/python_django/file_uploader/views.py
@@ -8,16 +8,14 @@ from file_uploader import settings
 from django.views.decorators.csrf import csrf_exempt
 from file_uploader import qqFileUploader
 
+
 def index(request):
-    
     return render_to_response('demo.htm')
+
 
 @csrf_exempt
 def upload(request):
-
-    allowedExtension = [".jpg",".png",".ico",".*"]
+    allowedExtension = [".jpg", ".png", ".ico", ".*"]
     sizeLimit = 1024000
-    
-    uploader = qqFileUploader(allowedExtension,sizeLimit)
-    
+    uploader = qqFileUploader(allowedExtension, sizeLimit)
     return HttpResponse(uploader.handleUpload(request, settings.MEDIA_ROOT + "upload/"))
