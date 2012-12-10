72e6e166bbc7ef5ce772d1375a8526ee5f57a289
Ray Nicholus <rnicholus@widen.com>
2012-12-09 21:32:10 -0600

    #514 - ensure error message is returned as valid JSON

--- server/php.php
+++ server/php.php
@@ -162,7 +162,7 @@ class qqFileUploader {
         
         if ($postSize < $this->sizeLimit || $uploadSize < $this->sizeLimit){
             $size = max(1, $this->sizeLimit / 1024 / 1024) . 'M';             
-            die("{'error':'increase post_max_size and upload_max_filesize to $size'}");    
+            die(json_encode(array('error'=>'increase post_max_size and upload_max_filesize to ' . $size)));    
         }        
     }
     
