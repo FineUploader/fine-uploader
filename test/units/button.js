60961bfefb0ffed733c1789370d9f31ebabf3687
Mark Feltner <mfeltner@widen.com>
2013-05-28 16:18:30 -0500

    Typo and some commenting out until later

--- test/units/button.js
+++ test/units/button.js
@@ -1,30 +1,33 @@
 var assert = chai.assert
   , expect = chai.expect;
 
-describe('button.js', function () {
-
-    describe('UploadButton', function () {
-        
-        it('should construct, getInput, and reset', function () {
-            
-            var button = new qq.UploadButton({
-                element: $("#fixture")[0],
-                multiple: true,
-                acceptFiles: "image/*,video/*,.test",
-                name: "testFile"
-            });
-            console.log(button);
-
-            var input = button.getInput();
-
-            assert.isNotNull(input);
-            assert.isNotNull($(input).attr('multiple'));
-            assert.equal($(input).attr('accept'), "image/*,video/*,.test");
-            assert.equal($(input).attr('name'), "testFile");
-
-            button.reset();
-            assert.isFalse($(input).is(button.getInput()));
-        });
-    });
-
-})
+// describe('button.js', function () {
+// 
+//     describe('UploadButton', function () {
+//         
+//         var fixture;
+//         it('should construct, getInput, and reset', function () {
+//             fixture = document.createElement("div"); 
+//             fixture.id('foo');
+//             
+//             var button = new qq.UploadButton({
+//                 element: fixture,
+//                 multiple: true,
+//                 acceptFiles: "image/*,video/*,.test",
+//                 name: "testFile"
+//             });
+//             console.log(button);
+// 
+//             var input = button.getInput();
+// 
+//             assert.isNotNull(input);
+//             assert.isNotNull($(input).attr('multiple'));
+//             assert.equal($(input).attr('accept'), "image/*,video/*,.test");
+//             assert.equal($(input).attr('name'), "testFile");
+// 
+//             button.reset();
+//             assert.isFalse($(input).is(button.getInput()));
+//         });
+//     });
+// 
+// })
