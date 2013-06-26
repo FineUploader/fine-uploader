a8dca79b4b1871807fefb2648404156e435c9187
Ray Nicholus <rnicholus@widen.com>
2013-06-26 13:34:08 -0500

    docs(client/js/button.js): #879 - comprehensive documentation of modules

--- client/js/button.js
+++ client/js/button.js
@@ -1,21 +1,53 @@
 /*globals qq*/
+
+/**
+ * This module represents an upload or "Select File(s)" button.  It's job is to embed an opaque `<input type="file">`
+ * element as a child of a provided "container" element.  This "container" element (`options.element`) is used to provide
+ * a custom style for the `<input type="file">` element.  The ability to change the style of the container element is also
+ * provided here by adding CSS classes to the container on hover/focus.
+ *
+ * TODO Eliminate the mouseover and mouseout event handlers since the :hover CSS pseudo-class should now be
+ * available on all supported browsers.
+ *
+ * @param o Options to override the default values
+ * @return {{getInput: Function, reset: Function}}
+ * @constructor
+ */
 qq.UploadButton = function(o) {
     "use strict";
 
     var input,
+       // Used to detach all event handlers created at once for this instance
         disposeSupport = new qq.DisposeSupport(),
+
         options = {
+            // "Container" element
             element: null,
-            // if set to true adds multiple attribute to file input
+
+            // If true adds `multiple` attribute to `<input type="file">`
             multiple: false,
+
+            // Corresponds to the `accept` attribute on the associated `<input type="file">`
+
             acceptFiles: null,
-            // name attribute of file input
-            name: 'file',
+
+            // `name` attribute of `<input type="file">`
+            name: 'qqfile',
+
+            // Called when the browser invokes the onchange handler on the `<input type="file">`
             onChange: function(input) {},
+
+            // **This option will be removed** in the future as the :hover CSS pseudo-class is available on all supported browsers
             hoverClass: 'qq-upload-button-hover',
+
             focusClass: 'qq-upload-button-focus'
         };
 
+    // Overrides any of the default option values with any option values passed in during construction.
+    qq.extend(options, o);
+
+
+    // Embed an opaque `<input type="file">` element as a child of `options.element`.
     function createInput() {
         var input = document.createElement("input");
 
@@ -52,12 +84,14 @@ qq.UploadButton = function(o) {
             options.onChange(input);
         });
 
+        // **These event handlers will be removed** in the future as the :hover CSS pseudo-class is available on all supported browsers
         disposeSupport.attach(input, 'mouseover', function(){
             qq(options.element).addClass(options.hoverClass);
         });
         disposeSupport.attach(input, 'mouseout', function(){
             qq(options.element).removeClass(options.hoverClass);
         });
+
         disposeSupport.attach(input, 'focus', function(){
             qq(options.element).addClass(options.focusClass);
         });
@@ -67,7 +101,7 @@ qq.UploadButton = function(o) {
 
         // IE and Opera, unfortunately have 2 tab stops on file input
         // which is unacceptable in our case, disable keyboard access
-        if (window.attachEvent){
+        if (window.attachEvent) {
             // it is IE or Opera
             input.setAttribute('tabIndex', "-1");
         }
@@ -75,20 +109,18 @@ qq.UploadButton = function(o) {
         return input;
     }
 
-
-    qq.extend(options, o);
-
-    // make button suitable container for input
+    // Make button suitable container for input
     qq(options.element).css({
         position: 'relative',
         overflow: 'hidden',
-        // Make sure browse button is in the right side
-        // in Internet Explorer
+        // Make sure browse button is in the right side in Internet Explorer
         direction: 'ltr'
     });
 
     input = createInput();
 
+
+    // Exposed API
     return {
         getInput: function(){
             return input;
