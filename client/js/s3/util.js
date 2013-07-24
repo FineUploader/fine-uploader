b7ceeee66dcd0025d297dd414a036f77ac60b945
Ray Nicholus <rnicholus@widen.com>
2013-07-24 16:13:38 -0500

    feat(client/js/s3/util.js): #899 - use MDN date.toISOString shim instead

--- client/js/s3/util.js
+++ client/js/s3/util.js
@@ -164,17 +164,24 @@ qq.s3.util = qq.s3.util || (function() {
                 return date.toISOString();
             }
             else {
-                function padZeros(n) {
-                    return n < 10 ? '0' + n : n
+                function pad(number) {
+                    var r = String(number);
+
+                    if ( r.length === 1 ) {
+                        r = '0' + r;
+                    }
+
+                    return r;
                 }
 
-                return         date.getUTCFullYear() + '-'
-                    + padZeros(date.getUTCMonth() + 1) + '-'
-                    + padZeros(date.getUTCDate()) + 'T'
-                    + padZeros(date.getUTCHours()) + ':'
-                    + padZeros(date.getUTCMinutes()) + ':'
-                    + padZeros(date.getUTCSeconds()) + 'Z';
-            }
+                return this.getUTCFullYear()
+                        + '-' + pad( this.getUTCMonth() + 1 )
+                        + '-' + pad( this.getUTCDate() )
+                        + 'T' + pad( this.getUTCHours() )
+                        + ':' + pad( this.getUTCMinutes() )
+                        + ':' + pad( this.getUTCSeconds() )
+                        + '.' + String( (this.getUTCMilliseconds()/1000).toFixed(3) ).slice( 2, 5 )
+                        + 'Z';            }
         },
 
         /**
