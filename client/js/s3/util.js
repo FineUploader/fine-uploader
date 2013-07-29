4018c0218f69119a481ae663ff71629bf6b6c5ac
Ray Nicholus <rnicholus@widen.com>
2013-07-29 10:11:29 -0500

    fix(client/js/s3/util.js): #899 - date formatting util code typo

--- client/js/s3/util.js
+++ client/js/s3/util.js
@@ -174,13 +174,13 @@ qq.s3.util = qq.s3.util || (function() {
                     return r;
                 }
 
-                return this.getUTCFullYear()
-                        + '-' + pad( this.getUTCMonth() + 1 )
-                        + '-' + pad( this.getUTCDate() )
-                        + 'T' + pad( this.getUTCHours() )
-                        + ':' + pad( this.getUTCMinutes() )
-                        + ':' + pad( this.getUTCSeconds() )
-                        + '.' + String( (this.getUTCMilliseconds()/1000).toFixed(3) ).slice( 2, 5 )
+                return date.getUTCFullYear()
+                        + '-' + pad( date.getUTCMonth() + 1 )
+                        + '-' + pad( date.getUTCDate() )
+                        + 'T' + pad( date.getUTCHours() )
+                        + ':' + pad( date.getUTCMinutes() )
+                        + ':' + pad( date.getUTCSeconds() )
+                        + '.' + String( (date.getUTCMilliseconds()/1000).toFixed(3) ).slice( 2, 5 )
                         + 'Z';            }
         },
 
