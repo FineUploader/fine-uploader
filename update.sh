5773102eacc7b53a881734ed000eb44d352e74af
Ray Nicholus <rnicholus@widen.com>
2013-09-06 12:03:49 -0500

    chore(update.sh): increment build number on push to develop

---
+++ update.sh
@@ -0,0 +1,16 @@
+###
+# This will update the build number as part of each push (only in the develop branch).
+#
+# To install this hook:
+# 1.) chmod +x update.sh
+# 2.) ln -s -f ../../update.sh .git/hooks/update
+###
+BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+if [ $BRANCH == "develop" ]; then
+    grunt version:build
+    RESULT=$?
+    [ $RESULT -ne 0 ] && exit 1
+    git add -u
+    git commit -a -m "update version"
+    exit 0
+fi
