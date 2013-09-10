f53b6d4690210c9421269310b183d324ab94263b
Mark Feltner <feltner.mj@gmail.com>
2013-09-10 11:03:33 -0500

    test(iframe.xss.response.js): Proper tests
    Actually tests iframe postMessage and receive message events along
    with the new iframe.xss.response parsing code.
    
    References: #913

--- test/unit/iframe.xss.response.js
+++ test/unit/iframe.xss.response.js
@@ -1,27 +1,67 @@
-describe('iframe.xss.response', function () {
+if (window.postMessage && !window.PHANTOMJS) {
+    describe('iframe.xss.response', function () {
 
-    before(function () {
-        $('body').append("<script id='iframe-xss-response' src='temp/iframe.xss.response.js'></script>");
+        var iframe, doc;
+        var script = '<scr' + 'ipt type="text/javascript" src="/test/temp/iframe.xss.response.js"></scr' + 'ipt>';
+
+        beforeEach(function () {
+            iframe = document.createElement('iframe');
+            iframe.setAttribute('id', 'iframe-fixture');
+            iframe.setAttribute('src', 'java' + String.fromCharCode(115) + 'cript:false;');
+            $("#mocha-fixture").append(iframe);
+            doc = iframe.contentWindow || iframe.contentDocument;
+            if (doc.document) {
+                doc = doc.document;
+            }
+        });
+
+        afterEach(function () {
+            $("#iframe-fixture").remove();
+            doc = null;
+            $(window).off('message');
+        });
+
+        it('#913 - Correctly parses JSON nested more than 1 level (simple)', function (done) {
+            var stuff = JSON.stringify({ hello: 'world', my: 'name', is: 'mark' });
+
+            $(window).on('message', function (event) {
+                var data = event.originalEvent.data;
+                assert.equal(data, stuff);
+                done();
+            });
+
+            doc.open();
+            doc.write(stuff + script);
+            doc.close();
+        });
+
+        it('#913 - Correctly parses JSON nested more than 1 level (deep)', function (done) {
+            var stuff = JSON.stringify({ hello: { my: 'name', is: { mark: '?' }}, foo: 'bar'});
+
+            $(window).on('message', function (event) {
+                var data = event.originalEvent.data;
+                assert.equal(data, stuff);
+                done();
+            });
+
+            doc.open();
+            doc.write(stuff + script);
+            doc.close();
+        });
+
+        it('#913 - Correctly parses JSON nested more than 1 level (test case)', function (done) {
+            var stuff = '{"images":{"id":2}, "uuid":"bla"}';
+
+            $(window).on('message', function (event) {
+                var data = event.originalEvent.data;
+                assert.equal(data, stuff);
+                done();
+            });
+
+            doc.open();
+            doc.write(stuff + script);
+            doc.close();
+        });
+
     });
-
-    after(function () {
-        $("#iframe-xss-response").remove();
-    });
-
-    it('#913 - Has matching regex for JSON nested more than 1 level', function () {
-       var simpleStuff, deepStuff, match;
-
-       simpleStuff = JSON.stringify({ hello: 'world', my: 'name', is: 'mark' });
-       match = readIframeResponse(simpleStuff);
-       assert.equal(match[1], simpleStuff);
-
-       deepStuff = JSON.stringify({ hello: { my: 'name', is: { mark: '?' }}, foo: 'bar'});
-       match = readIframeResponse(deepStuff);
-       assert.ok(match[1], deepStuff);
-
-       moarStuff = '{"images":{"id":2}, "uuid":"bla"}';
-       match = readIframeResponse(moarStuff);
-       assert.ok(match[1], moarStuff);
-    });
-
-});
+}
