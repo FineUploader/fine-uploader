69f788c8c4610e8e88a304f0a1668e5d6d006a75
Mark Feltner <feltner.mj@gmail.com>
2013-09-11 17:31:15 -0500

    fix(test/unit/iframe.xss.response.js): Fix path to script
    Karma puts scripts into a './base' folder

--- test/unit/iframe.xss.response.js
+++ test/unit/iframe.xss.response.js
@@ -2,7 +2,7 @@ if (window.postMessage) {
     describe('iframe.xss.response', function () {
 
         var iframe, doc;
-        var script = '<scr' + 'ipt type="text/javascript" src="temp/iframe.xss.response.js"></scr' + 'ipt>';
+        var script = '<scr' + 'ipt type="text/javascript" src="/base/client/js/iframe.xss.response.js"></scr' + 'ipt>';
 
         beforeEach(function () {
             iframe = document.createElement('iframe');
