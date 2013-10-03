608a660838c8cf24b854d1571e2bd8003fca977a
Ray Nicholus <rnicholus@widen.com>
2013-10-03 12:35:47 -0500

    feat(client/js/third-party/megapix.image.js): #868 - same CPU cycles on non-iOS
    [skip ci]

--- client/js/third-party/megapix-image.js
+++ client/js/third-party/megapix-image.js
@@ -76,35 +76,43 @@
     var ctx = canvas.getContext('2d');
     ctx.save();
     transformCoordinate(canvas, width, height, options.orientation);
-    var subsampled = detectSubsampling(img);
-    if (subsampled) {
-      iw /= 2;
-      ih /= 2;
+
+    // Fine Uploader specific: Save some CPU cycles if not using iOS
+    // Assumption: This logic is only needed to overcome iOS image sampling issues
+    if (qq.ios()) {
+        var subsampled = detectSubsampling(img);
+        if (subsampled) {
+          iw /= 2;
+          ih /= 2;
+        }
+        var d = 1024; // size of tiling canvas
+        var tmpCanvas = document.createElement('canvas');
+        tmpCanvas.width = tmpCanvas.height = d;
+        var tmpCtx = tmpCanvas.getContext('2d');
+        var vertSquashRatio = doSquash ? detectVerticalSquash(img, iw, ih) : 1;
+        var dw = Math.ceil(d * width / iw);
+        var dh = Math.ceil(d * height / ih / vertSquashRatio);
+        var sy = 0;
+        var dy = 0;
+        while (sy < ih) {
+          var sx = 0;
+          var dx = 0;
+          while (sx < iw) {
+            tmpCtx.clearRect(0, 0, d, d);
+            tmpCtx.drawImage(img, -sx, -sy);
+            ctx.drawImage(tmpCanvas, 0, 0, d, d, dx, dy, dw, dh);
+            sx += d;
+            dx += dw;
+          }
+          sy += d;
+          dy += dh;
+        }
+        ctx.restore();
+        tmpCanvas = tmpCtx = null;
     }
-    var d = 1024; // size of tiling canvas
-    var tmpCanvas = document.createElement('canvas');
-    tmpCanvas.width = tmpCanvas.height = d;
-    var tmpCtx = tmpCanvas.getContext('2d');
-    var vertSquashRatio = doSquash ? detectVerticalSquash(img, iw, ih) : 1;
-    var dw = Math.ceil(d * width / iw);
-    var dh = Math.ceil(d * height / ih / vertSquashRatio);
-    var sy = 0;
-    var dy = 0;
-    while (sy < ih) {
-      var sx = 0;
-      var dx = 0;
-      while (sx < iw) {
-        tmpCtx.clearRect(0, 0, d, d);
-        tmpCtx.drawImage(img, -sx, -sy);
-        ctx.drawImage(tmpCanvas, 0, 0, d, d, dx, dy, dw, dh);
-        sx += d;
-        dx += dw;
-      }
-      sy += d;
-      dy += dh;
+    else {
+        ctx.drawImage(img, 0, 0, width, height);
     }
-    ctx.restore();
-    tmpCanvas = tmpCtx = null;
   }
 
   /**
