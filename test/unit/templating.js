3aac1a13ef6b6d665352c55789f52a9bfe2b7297
Ray Nicholus <rnicholus@widen.com>
2013-10-14 16:17:19 -0500

    test(test/unit/templating.js): fix templating test

--- test/unit/templating.js
+++ test/unit/templating.js
@@ -1,10 +1,10 @@
-describe.skip("templating.js", function() {
+describe("templating.js", function() {
     var templating;
 
     beforeEach(function() {
-        var $template = $('<script id="qq-template"></script>');
-        $template.text('<div class="qq-uploader-selector qq-uploader"><ul class="qq-upload-list-selector qq-upload-list"></ul></div>');
-        $("body").append($template);
+        var $template = $('<script id="qq-template" type="text/template"></script>');
+        $template.html('<div class="qq-uploader-selector qq-uploader"><ul class="qq-upload-list-selector qq-upload-list"></ul></div>');
+        $fixture.append($template);
         templating = new qq.Templating({});
     });
 
