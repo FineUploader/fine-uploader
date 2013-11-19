FineUploader/fine-uploader

commit 9994e562ef81973bb3c9907cc49a1ab3fa962f09

fix($build): Stop jQuery plugin from being concat twice
Only affected using jQuery + S3

lib/modules.js
@@ -94,7 +94,6 @@ var fineUploaderModules = {
     "client/js/s3/uploader.js",
   ],
   "fuSrcS3Jquery": [
-    "@fuSrcJquery",
     "client/js/s3/jquery-plugin.js"
   ],
   "fuImagePreviewModule": [

commit eb7c6ef3b16935a06ebe0b9b703e93de17770f8f

fix($build): Add jQuery to S3+jQuery builds
The `@fuSrcS3Jquery` modulew was not being prepended with the `@fuSrcJquery` module.
This was a regression caused in 9994e562ef81973bb3c9907cc49a1ab3fa962f09
in order to prevent double loading of modules.

Now, the list of modules are constructed and then filtered to only contain unique
modules (to prevent duplicates).

lib/modules.js
@@ -3,6 +3,9 @@
 // shamelessly inspired by:
 // https://github.com/angular/angular.js/blob/master/angularFiles.js
 
+var //dependencies
+    _  = require('underscore');
+
 var fineUploaderModules = {
   // Pre-defined forumlae
   "fuTraditional": [
@@ -94,6 +97,7 @@ var fineUploaderModules = {
     "client/js/s3/uploader.js",
   ],
   "fuSrcS3Jquery": [
+    "@fuSrcJquery",
     "client/js/s3/jquery-plugin.js"
   ],
   "fuImagePreviewModule": [
@@ -202,7 +206,7 @@ if (exports) {
       });
     });
 
-    return files;
+    return _.unique(files);
   };
 
   exports.modules = fineUploaderModules;