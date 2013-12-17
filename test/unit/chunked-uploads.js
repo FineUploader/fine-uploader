219f583f28847d7bb6c052e795257dd7e5a408d4
Ray Nicholus <rnicholus@widen.com>
2013-12-17 10:32:10 -0600

    test(unit/chunked-uploads.js): #1063 chunked upload tests

---
+++ test/unit/chunked-uploads.js
@@ -0,0 +1,251 @@
+if (qqtest.canDownloadFileAsBlob) {
+    describe("chunked uploads", function() {
+        var testUploadEndpoint = "/test/upload",
+            xhr,
+            oldWrapCallbacks,
+            requests,
+            expectedFileSize = 3266,
+            expectedChunks = 3,
+            chunkSize = Math.round(expectedFileSize / expectedChunks),
+            params = {
+                foo: "bar",
+                one: 2
+            },
+            overridenChunkingParamNames = {
+                chunkSize: "testchunksize",
+                partByteOffset: "testpartbyteoffset",
+                partIndex: "testpartindex",
+                totalParts: "testtotalparts"
+            };
+
+        beforeEach(function() {
+            mockFormData();
+
+            requests = [];
+            oldWrapCallbacks = qq.FineUploaderBasic.prototype._wrapCallbacks;
+
+            // "Turn off" wrapping of callbacks that squelches errors.  We need AssertionErrors in callbacks to bubble.
+            qq.FineUploaderBasic.prototype._wrapCallbacks = function() {};
+        });
+
+        afterEach(function() {
+            unmockXhr();
+            unmockFormData();
+
+            qq.FineUploaderBasic.prototype._wrapCallbacks = oldWrapCallbacks;
+        });
+
+        function mockXhr() {
+            xhr = sinon.useFakeXMLHttpRequest();
+            xhr.onCreate = function(req) {
+                requests.push(req);
+            };
+        }
+
+        function unmockXhr() {
+            xhr && xhr.restore();
+        }
+
+        function testChunkedUpload(mpe, customParams, chunkingParamNames, done) {
+            customParams = customParams || {};
+            chunkingParamNames = chunkingParamNames || new qq.FineUploaderBasic({})._options.chunking.paramNames;
+
+            assert.expect(2 + (expectedChunks * (14 + (Object.keys(customParams).length))), done);
+
+            var uploader = new qq.FineUploaderBasic({
+                    request: {
+                        endpoint: testUploadEndpoint,
+                        paramsInBody: mpe,
+                        forceMultipart: mpe,
+                        params: customParams
+                    },
+                    chunking: {
+                        enabled: true,
+                        partSize: chunkSize,
+                        paramNames: chunkingParamNames
+                    },
+                    callbacks: {
+                        onUploadChunk: function (id, name, chunkData) {
+                            var request = requests[requests.length - 1];
+
+                            chunksSent++;
+
+                            assert.equal(id, 0, "Wrong ID passed to onUpoadChunk");
+                            assert.equal(name, uploader.getName(id), "Wrong name passed to onUploadChunk");
+                            assert.equal(chunkData.partIndex, chunksSent - 1, "Wrong partIndex passed to onUploadChunk");
+                            assert.equal(chunkData.startByte, (chunksSent - 1) * chunkSize + 1, "Wrong startByte passed to onUploadChunk");
+                            assert.equal(chunkData.endByte, chunksSent === expectedChunks ? expectedFileSize : chunkData.startByte + chunkSize - 1, "Wrong startByte passed to onUploadChunk");
+                            assert.equal(chunkData.totalParts, expectedChunks, "Wrong totalParts passed to onUploadChunk");
+
+                            setTimeout(function () {
+                                request.respond(200, null, JSON.stringify({success: true}));
+                            }, 10);
+                        },
+                        onUploadChunkSuccess: function (id, chunkData, response, xhr) {
+                            var request = requests[requests.length - 1],
+                                requestParams;
+
+                            chunksSucceeded++;
+
+                            if (mpe) {
+                                requestParams = request.requestBody.fields;
+                            }
+                            else {
+                                requestParams = purl(request.url).param();
+                            }
+
+                            assert.equal(requestParams.qquuid, uploader.getUuid(id), "Wrong uuid param");
+                            assert.equal(requestParams[chunkingParamNames.partIndex], chunksSent - 1, "Wrong part index param");
+                            assert.equal(requestParams[chunkingParamNames.partByteOffset], (chunksSent - 1) * chunkSize, "Wrong part byte offset param");
+                            assert.equal(requestParams.qqtotalfilesize, expectedFileSize, "Wrong total file size param");
+                            assert.equal(requestParams[chunkingParamNames.totalParts], expectedChunks, "Wrong total parts param");
+                            assert.equal(requestParams.qqfilename, uploader.getName(id), "Wrong filename param");
+                            assert.equal(requestParams[chunkingParamNames.chunkSize], mpe ? requestParams.qqfile.size : request.requestBody.size, "Wrong chunk size param");
+                            assert.equal(id, 0, "Wrong ID passed to onUpoadChunkSuccess");
+
+                            qq.each(customParams, function(key, val) {
+                                assert.equal(requestParams[key], val, qq.format("Wrong value for {} param", key));
+                            });
+
+                            // TODO uncomment these when #1073 is fixed.
+//                        assert.equal(chunkData.partIndex, chunksSucceeded - 1, "Wrong partIndex passed to onUploadChunkSuccess");
+//                        assert.equal(chunkData.startByte, (chunksSent - 1) * chunkSize + 1, "Wrong startByte passed to onUploadChunkSuccess");
+//                        assert.equal(chunkData.endByte, chunksSucceeded === expectedChunks ? expectedFileSize : chunkData.startByte + chunkSize-1, "Wrong startByte passed to onUploadChunk");
+//                        assert.equal(chunkData.totalParts, expectedChunks, "Wrong totalParts passed to onUploadChunkSuccess");
+                        },
+                        onComplete: function (id) {
+                            assert.equal(expectedChunks, chunksSent, "Wrong # of chunks sent.");
+                            assert.equal(expectedChunks, chunksSucceeded, "Wrong # of chunks succeeded");
+                        }
+                    }
+                }),
+                chunksSent = 0,
+                chunksSucceeded = 0;
+
+            qqtest.downloadFileAsBlob("up.jpg", "image/jpeg").then(function (blob) {
+                mockXhr();
+                uploader.addBlobs({name: "test", blob: blob});
+            });
+        }
+
+        function testChunkedFailureAndRecovery(restartAfterFailure, done) {
+            if (restartAfterFailure) {
+                assert.expect(5 + (expectedChunks * 16) + (14 * (expectedChunks-1)), done);
+            }
+            else {
+                assert.expect(5 + (expectedChunks * 16), done);
+            }
+
+            var alreadyFailed = false,
+                uploader = new qq.FineUploaderBasic({
+                    request: {
+                        endpoint: testUploadEndpoint
+                    },
+                    chunking: {
+                        enabled: true,
+                        partSize: chunkSize
+                    },
+                    retry: {
+                        autoAttemptDelay: 0,
+                        enableAuto: true
+                    },
+                    callbacks: {
+                        onUploadChunk: function (id, name, chunkData) {
+                            var request = requests[requests.length - 1];
+
+                            chunksSent++;
+
+                            assert.equal(id, 0, "Wrong ID passed to onUpoadChunk");
+                            assert.equal(name, uploader.getName(id), "Wrong name passed to onUploadChunk");
+                            assert.equal(chunkData.partIndex, chunksSent - 1, "Wrong partIndex passed to onUploadChunk");
+                            assert.equal(chunkData.startByte, (chunksSent - 1) * chunkSize + 1, "Wrong startByte passed to onUploadChunk");
+                            assert.equal(chunkData.endByte, chunksSent === expectedChunks ? expectedFileSize : chunkData.startByte + chunkSize - 1, "Wrong startByte passed to onUploadChunk");
+                            assert.equal(chunkData.totalParts, expectedChunks, "Wrong totalParts passed to onUploadChunk");
+
+                            setTimeout(function () {
+                                if (chunksSent === expectedChunks && !alreadyFailed) {
+                                    alreadyFailed = true;
+
+                                    if (restartAfterFailure) {
+                                        chunksSent = 0;
+                                        chunksSucceeded = 0;
+                                        request.respond(500, null, JSON.stringify({reset: true}));
+                                    }
+                                    else {
+                                        chunksSent--;
+                                        request.respond(500, null);
+                                    }
+                                }
+                                else {
+                                    request.respond(200, null, JSON.stringify({success: true}));
+                                }
+                            }, 10);
+                        },
+                        onAutoRetry: function(id, name, attemptNumber) {
+                            assert.equal(id, 0, "Wrong ID passed to onAutoRetry");
+                            assert.equal(name, uploader.getName(id), "Wrong name passed to onAutoRetry");
+                            assert.equal(attemptNumber, 1, "Wrong auto retry attempt #");
+                        },
+                        onUploadChunkSuccess: function (id, chunkData, response, xhr) {
+                            var request = requests[requests.length - 1],
+                                requestParams = request.requestBody.fields;
+
+                            chunksSucceeded++;
+
+                            assert.equal(requestParams.qquuid, uploader.getUuid(id), "Wrong uuid param");
+                            assert.equal(requestParams.qqpartindex, chunksSent - 1, "Wrong part index param");
+                            assert.equal(requestParams.qqpartbyteoffset, (chunksSent - 1) * chunkSize, "Wrong part byte offset param");
+                            assert.equal(requestParams.qqtotalfilesize, expectedFileSize, "Wrong total file size param");
+                            assert.equal(requestParams.qqtotalparts, expectedChunks, "Wrong total parts param");
+                            assert.equal(requestParams.qqfilename, uploader.getName(id), "Wrong filename param");
+                            assert.equal(requestParams.qqchunksize, requestParams.qqfile.size, "Wrong chunk size param");
+                            assert.equal(id, 0, "Wrong ID passed to onUpoadChunkSuccess");
+                        },
+                        onComplete: function (id) {
+                            assert.equal(expectedChunks, chunksSent, "Wrong # of chunks sent.");
+                            assert.equal(expectedChunks, chunksSucceeded, "Wrong # of chunks succeeded");
+                        }
+                    }
+                }),
+                chunksSent = 0,
+                chunksSucceeded = 0;
+
+            qqtest.downloadFileAsBlob("up.jpg", "image/jpeg").then(function (blob) {
+                mockXhr();
+                uploader.addBlobs({name: "test", blob: blob});
+            });
+        }
+
+        it("sends proper number of chunks when chunking is enabled, MPE", function(done) {
+            testChunkedUpload(true, null, null, done);
+        });
+
+        it("sends proper number of chunks when chunking is enabled, non-MPE", function(done) {
+            testChunkedUpload(false, null, null, done);
+        });
+
+        it("sends custom parameters along with each chunk, MPE", function(done) {
+            testChunkedUpload(true, params, null, done);
+        });
+
+        it("sends custom parameters along with each chunk, non-MPE", function(done) {
+            testChunkedUpload(false, params, null, done);
+        });
+
+        it("specifies custom values for the various chunking parameters, MPE", function(done) {
+            testChunkedUpload(true, null, overridenChunkingParamNames, done);
+        });
+
+        it("specifies custom values for the various chunking parameters, non-MPE", function(done) {
+            testChunkedUpload(false, null, overridenChunkingParamNames, done);
+        });
+
+        it("fails the last chunk once, then recovers", function(done) {
+            testChunkedFailureAndRecovery(false, done);
+        });
+
+        it("fails the last chunk once, then restarts with the first chunk", function(done) {
+            testChunkedFailureAndRecovery(true, done);
+        });
+    });
+}
