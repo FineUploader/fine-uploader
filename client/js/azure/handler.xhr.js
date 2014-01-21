026aee53aaec9347ce7e30ea4e64dc855551c7ba
Ray Nicholus <rnicholus@widen.com>
2014-01-21 15:02:16 -0600

    feat($azure-xhr): Start of XHR upload handler for Azure - #1022

--- client/js/azure/handler.xhr.js
+++ client/js/azure/handler.xhr.js
@@ -10,4 +10,44 @@
 qq.azure.UploadHandlerXhr = function(spec, proxy) {
     "use strict";
 
+    var publicApi = this,
+        internalApi = {},
+        fileState = [],
+        log = proxy.log,
+        onProgress = spec.onProgress,
+        onComplete = spec.onComplete,
+        onUpload = spec.onUpload,
+        onUploadCompleted = function(id, xhr, isError) {
+            onComplete(id, getName(id), {success: true}, xhr);
+        },
+        getName = proxy.getName,
+        getUuid = proxy.getUuid,
+        getSize = proxy.getSize,
+        progressHandler = function(id, loaded, total) {
+            fileState[id].loaded = loaded;
+            onProgress(id, getName(id), loaded, total);
+        },
+        putBlob = new qq.azure.PutBlob({
+            onProgress: progressHandler,
+            onUpload: onUpload,
+            onComplete: onUploadCompleted,
+            log: log
+        });
+
+
+    function handleStartUploadSignal(id) {
+        var fileOrBlob = publicApi.getFile(id),
+            url = "about:blank", //TODO this will contain the SAS URI from the integrator's server,
+            xhr;
+
+        xhr = putBlob.upload(id, url, {}, fileOrBlob);
+        internalApi.registerXhr(id, xhr);
+    }
+
+    qq.extend(this, new qq.UploadHandlerXhrApi(
+        internalApi,
+        {fileState: fileState, chunking: false},
+        {onUpload: handleStartUploadSignal, onCancel: spec.onCancel, /*onUuidChanged: onUuidChanged,*/ getName: getName,
+            getSize: getSize, getUuid: getUuid, log: log}
+    ));
 };
