4ca20b0449366df7ae1e34f595b70b0432daa26b
Ray Nicholus <rnicholus@widen.com>
2014-01-21 15:03:41 -0600

    feat(js/azure/uploader.basic.js): Barebones FU Core impl for Azure - #1022

--- client/js/azure/uploader.basic.js
+++ client/js/azure/uploader.basic.js
@@ -14,23 +14,8 @@
         // Replace any default options with user defined ones
         qq.extend(options, o, true);
 
-        if (!this.setCredentials(options.credentials, true)) {
-            this._currentCredentials.accessKey = options.request.accessKey;
-        }
-
-        this._aclStore = this._createStore(options.objectProperties.acl);
-
         // Call base module
         qq.FineUploaderBasic.call(this, options);
-
-        this._uploadSuccessParamsStore = this._createStore(this._options.uploadSuccess.params);
-
-        // This will hold callbacks for failed uploadSuccess requests that will be invoked on retry.
-        // Indexed by file ID.
-        this._failedSuccessRequestCallbacks = {};
-
-        // Holds S3 keys for file representations constructed from a session request.
-        this._cannedKeys = {};
     };
 
     // Inherit basic public & private API methods.
@@ -39,5 +24,8 @@
 
     // Define public & private API methods for this module.
     qq.extend(qq.azure.FineUploaderBasic.prototype, {
+        _createUploadHandler: function() {
+            return qq.FineUploaderBasic.prototype._createUploadHandler.call(this, null, "azure");
+        }
     });
 }());
