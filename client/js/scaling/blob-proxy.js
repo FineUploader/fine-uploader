b844c15dbc2508cbe2b1320767600f913e2d19a7
Ray Nicholus <rnicholus@widen.com>
2014-02-12 13:35:48 -0600

    chore($scaling): name adjustments #1061

--- client/js/scaling/blob-proxy.js
+++ client/js/scaling/blob-proxy.js
@@ -2,16 +2,16 @@
 /**
  * Placeholder for a Blob that will be generated on-demand.
  *
- * @param originalFile Parent of the generated blob
+ * @param referenceBlob Parent of the generated blob
  * @param onCreate Function to invoke when the blob must be created.  Must be promissory.
  * @constructor
  */
-qq.BlobProxy = function(originalFile, onCreate) {
+qq.BlobProxy = function(referenceBlob, onCreate) {
     "use strict";
 
     qq.extend(this, {
         create: function() {
-            return onCreate(originalFile);
+            return onCreate(referenceBlob);
         }
     });
 };
