4d9ff60aba9fdbc8cbb63b832f73b7c027f1b116
Ray Nicholus <rnicholus@widen.com>
2014-02-28 12:59:07 -0600

    chore(test/uploader-demo-jquery.js): remove annoying validation from test js

--- test/uploader-demo-jquery.js
+++ test/uploader-demo-jquery.js
@@ -115,9 +115,6 @@
                             waitingPath: "/client/placeholders/waiting-generic.png",
                             notAvailablePath: "/client/placeholders/not_available-generic.png"
                         }
-                    },
-                    validation: {
-                        itemLimit: 3
                     }
                 }).on('error', errorHandler).on("upload", function(event, id, filename) {
                         $(this).fineUploader('setParams', {
