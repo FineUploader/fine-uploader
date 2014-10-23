4321f2ee7c4e3b13a527b18a04714f2c26356601
Ray Nicholus <rnicholus@widen.com>
2014-10-23 13:56:36 -0500

    fix(previews): No limits for older browsers.
    TODO:
    - Cleanup templating module code.
    
    - Don’t pause before next thumbnail generation
    attempt if we’ve already hit the maxCount.
    Only increment generatePreviews if an image
    without a valid preview has been updated
    via server’s thumbnail response.
    
    - Update styling doc.
    
    #1279

--- client/js/templating.js
+++ client/js/templating.js
@@ -574,6 +574,13 @@ qq.Templating = function(spec) {
     qq.extend(options, spec);
     log = options.log;
 
+    // No need to worry about conserving CPU or memory on older browsers,
+    // since there is no ability to preview, and thumbnail display is primitive and quick.
+    if (!qq.supportedFeatures.imagePreviews) {
+        options.limits.timeBetweenThumbs = 0;
+        options.limits.maxThumbs = 0;
+    }
+
     container = options.containerEl;
     showThumbnails = options.imageGenerator !== undefined;
     templateHtml = parseAndGetTemplate();
