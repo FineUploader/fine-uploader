ed6255fcd733770a8a3ffea0174e62dff8d79f21
Korijn van Golen <korijn@gmail.com>
2015-04-30 11:52:50 +0200

    Add unprefixed Azure request headers support
    Adds support for a predefined list of headers which are appropriate for
    Azure without the x-ms-meta- prefix.
    
    Resolves issue #1212 and is based on similar pull request #1258.

--- client/js/azure/util.js
+++ client/js/azure/util.js
@@ -6,11 +6,41 @@ qq.azure.util = qq.azure.util || (function() {
     return {
         AZURE_PARAM_PREFIX: "x-ms-meta-",
 
+        /** Create Prefixed request headers which are appropriate for Azure.
+         *
+         * If the request header is appropriate for Azure (e.g. Cache-Control) then pass
+         * it along without a metadata prefix. For all other request header parameter names,
+         * apply qq.azure.util.AZURE_PARAM_PREFIX before the name.
+         *
+         * @param name Name of the Request Header parameter to construct a (possibly) prefixed name.
+         * @returns {String} A valid Request Header parameter name.
+         */
+        _getPrefixedParamName: function (name) {
+            switch (name)
+            {
+                //
+                // Valid request headers (not sent by fine-uploader) which should be returned as-is (case-sensitive)
+                // see: https://msdn.microsoft.com/en-us/library/azure/dd179451.aspx
+                //
+                case "Cache-Control":
+                case "Content-Disposition":
+                case "Content-Encoding":
+                case "Content-MD5":
+                case "x-ms-blob-content-encoding":
+                case "x-ms-blob-content-disposition":
+                case "x-ms-blob-content-md5":
+                case "x-ms-blob-cache-control":
+                    return name;
+                default:
+                    return qq.azure.util.AZURE_PARAM_PREFIX + name;
+            }
+        },
+
         getParamsAsHeaders: function(params) {
             var headers = {};
 
             qq.each(params, function(name, val) {
-                var headerName = qq.azure.util.AZURE_PARAM_PREFIX + name;
+                var headerName = qq.azure.util._getPrefixedParamName(name);
 
                 if (qq.isFunction(val)) {
                     headers[headerName] = encodeURIComponent(String(val()));
